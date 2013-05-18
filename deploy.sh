d94791a6348f90b4f0f89b37bdd3264a09465446
Alan Solidum <me@alansolidum.com>
2013-05-18 12:34:01 -1000

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 git checkout master
-jekyll
+jekyll build
 git add -A .
 git commit -m "static content update"
 git checkout gh-pages
 rm -rf *
-for file in `git ls-tree master _site -r --name-only`; do
-  `git checkout master $file` && cp -r _site/* . && rm -rf _site && touch .nojekyll
-done
+git checkout master _site && cp -rf _site/ . && rm -rf _site && touch .nojekyll
+git add -A .
+git commit -m "site content update"
+git push origin gh-pages
+git checkout master
